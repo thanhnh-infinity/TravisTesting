1b3b8613f1c76d29ccd215c1633763b707453084
thanhnh-infinity <thanhnh2911@gmail.com>
2016-02-09 00:07:04 -0700

    Test WS

--- runTest.py
+++ runTest.py
@@ -1,4 +1,11 @@
-def sayHello():
-   print "Hello Word !"
+def testWebService():
+   import urllib2
+   wsResult = urllib2.urlopen("http://127.0.0.1:8080/index").read()
+   if (wsResult == "Hello World!"):
+       print "Pass"
+       exit(0)
+   if (wsResult == "It fails"):
+       print "Fail"
+       exit(1)
 
-sayHello();
+testWebService();
